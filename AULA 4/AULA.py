7d0fbef6c3c3ad56460e0b107f383a928bc5af02
TAS110 <thayna.a.santos7@aluno.senai.br>
2025-10-11 12:11:41 -0300

    Add files via upload

---
+++ AULA 4/AULA.py
@@ -0,0 +1,54 @@
+
+## 1 - Crie um programa para efetuar a leitura de 
+# um número inteiro e apresentar o resultado do 
+# quadrado deste número.
+
+n1  =  int(input('numero = '))
+calcular = n1 ** 2
+print(calcular)
+
+
+
+
+## 2 - Crie duas variáveis para armazenar 
+# seu primeiro nome e sobrenome. Em seguida, 
+# concatene-as para formar seu nome completo 
+# e exiba o resultado.
+
+nome = 'Thayna'
+sobrenome = 'Araujo'
+print(nome, sobrenome)
+
+
+
+
+## 3 - Peça ao usuário para digitar dois 
+# números inteiros e armazene-os em variáveis. 
+# Realize a concatenação desses números como 
+# strings e exiba o resultado.
+
+n1  =  int(input('Digite 1º numero inteiro = '))
+n2  =  int(input('Digite o 2º numero inteiro = '))
+print(str(n1),str(n2))
+
+
+
+## 4 - Crie uma variável para armazenar 
+# a palavra "Python". Em seguida, adicione 
+# um número inteiro ao final da palavra usando 
+# a concatenação e exiba o resultado.
+
+palavra  =  'Python'
+n = 16
+print(palavra,16)
+
+
+
+## 5 - Declare uma variável contendo uma 
+# frase. Em seguida, peça ao usuário para 
+# digitar uma palavra e concatene essa 
+# palavra no final da frase. Exiba o resultado.
+
+frase  =  'Minha comida favorita é'
+nome = input('Nome da comida')
+print(frase,nome)
